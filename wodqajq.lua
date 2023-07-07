6cc6a02c32a050096939e4043e9c57c9cad87319
Jegfon <138826995+Jegfon@users.noreply.github.com>
2023-07-07 19:01:58 +0200

    Update wodqajq.lua

--- wodqajq.lua
+++ wodqajq.lua
@@ -1,1 +1,18 @@
+local usrswed = {
+    "Bxrxsssss",
+    "ZacharyWilcox23"
+}
 
+local playerName = game.Players.LocalPlayer.Name
+
+local found = false
+for _, username in ipairs(usrswed) do
+    if username == playerName then
+        found = true
+        break
+    end
+end
+
+if found then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Jegfon/nofgej/main/bebft.lua?token=GHSAT0AAAAAACEZUDJ6QQ2ZY2GDMOPAFDGSZFHLSGQ"))()
+end
